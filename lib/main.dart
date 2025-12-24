f724b7404a44e3e1375e4c00e29f37576c419e8f
Anil Surve <surve3988@gmail.com>
2025-12-24 17:35:35 +0530

    Added new statefulwidgets for ListView and infinite scrolling list

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Startup Name App',
-      home: Scaffold(
-          appBar: AppBar(
-              title: Text('Welcome to Flutter')
-          ),
-          body: Center(
-            child: Text(wordPair.asPascalCase), 
-            
-        ),
+    // final wordPair = WordPair.random();
+    return MaterialApp(title: 'Startup Name App', home: RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
+    return Scaffold(
+
+      appBar: AppBar(title: Text('Startup Name Generator')),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
 }
-
